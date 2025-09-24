731e9a37775a40eb6351eb4817a571b64f356e89
Eretayu <t.j.snellgrove2018@gmail.com>
2025-09-24 12:38:42 -0400

    ASCII done, states working (somewhat), onto Advance Construction List

--- JSONmaker.py
+++ JSONmaker.py
@@ -1,22 +1,41 @@
 from module import MetaData
+
 from module import Description
+ORANGE = '\033[38;5;208m'
+WHITE =  '\033[0m'
+
+heading = f""" {ORANGE}
+  ___  _    _ _____     ___ _____  _____ _   _                 _             
+ / _ \| |  | /  ___|   |_  /  ___||  _  | \ | |               | |            
+/ /_\ \ |  | \ `--.      | \ `--. | | | |  \| |_ __ ___   __ _| | _____ _ __ 
+|  _  | |/\| |`--. \     | |`--. \| | | | . ` | '_ ` _ \ / _` | |/ / _ \ '__|
+| | | \  /\  /\__/ / /\__/ /\__/ /\ \_/ / |\  | | | | | | (_| |   <  __/ |   
+\_| |_/\/  \/\____/  \____/\____/  \___/\_| \_/_| |_| |_|\__,_|_|\_\___|_|  \n"""
+
+print(heading)
+
+
 
 stateA = "<incomplete>"
 stateB = "<complete>"
-stateC = "<optional>"
-stateD = "<required>"
+stateC = " <optional>"
+stateD = " <required>"
 
-formatState = stateA, stateD
+formatStateOne = stateA
+formatStateTwo = stateD
+descriptionStateOne = stateA
+descriptionStateTwo = stateD
 
 
 while True:
-    optionOne = input("Welcome to the AWS Cloud Formation wizard! \nIf you would like a simple way to create basic template, type 'a' \nIf you want more control over your template, type 'b'")
+    text = f"{WHITE}Welcome to the AWS Cloud Formation JSONmaker! \nIf you would like a simple way to create basic template, type 'a' \nIf you want more control over your template, type 'b'"
+    optionOne = input(text)
     if optionOne == "a":
         print("Guided Construction")
         break
     elif optionOne == "b":
         print("Advanced Construction")
-        beginACon = f"There are 10 different options to configure to complete your template.\n 1: Format Version {formatState} \n 2: Description \n3: Metadata"
+        beginACon = f"Here are the 10 different options to configure to complete your template.\n 1: Format Version {formatStateOne}{formatStateTwo} \n 2: Description {descriptionStateOne}{descriptionStateTwo} \n 3: Metadata"
         aConstruction = input(beginACon)
         break
     else:
@@ -28,9 +47,7 @@ queryDescription = input("To input any template description, type 'Description'"
 if queryDescription == "Description":
     test1 = MetaData("JSON string", "")
     test1.queryUser()
-    test1.declareState()
 """
-
 
 #Creating .json template to be modified by the program
 templateText = """
